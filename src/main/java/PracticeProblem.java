4e3cf248aba5626652a06c31799a9776eb1606b1
Marcotian0211 <350327743@tdsb.ca>
2026-03-29 17:09:35 +0000

    update practice problem3-1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+     Scanner scanner=new Scanner(System.in);
 
 	}
+    public static String evenOrOdd(int num){
+        if(num % 2 == 0){
+            return "Even";}
+		 else {
+            return "Odd";
+        }
+    }	 	 
+	public static String teacherOrStudent(String word){
+		if(word=="Kalisz"){
+		  return "Teacher";}
+		else{
+		  return "Student";
+		}}
+	public static int fartherFromZero(int num){
+    if(num>0){
+		return (num+5);
+	}
+	if (num==0){
+		return 0;
+	}
+	else{
+		return num-5;
+	}
+	}
+	}
 
-}
+
+
+
